5be6b0b1480e028c1c31700a6f7417166844f151
Ravin Rakholiya <ravinrakholiya@Ravinmacpro.local>
2023-06-17 16:03:36 -0400

    initial setup for CanLi Flutter App

---
+++ android/app/src/main/java/com/example/canli/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.canli;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('CanLi'),
+        backgroundColor: const Color(0xFF1D2749),
+      ),
+
+    )
+  ));
+}
